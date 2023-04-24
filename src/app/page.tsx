8ff784fe3744923a53fe100dba8853249aed7dcf
Urban Vidovič <urbanfoundit@gmail.com>
2023-04-24 11:53:31 +0200

    fix: responsive design on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
     <main className=" font-cabin items-center flex justify-center text-gray-200 dark:text-deep px-12">
       <div
         id="mainContent"
-        className="sm:flex sm:flex-row mt-12 m-auto max-w-5xl justify-center"
+        className="sm:flex sm:flex-row sm:m-auto mt-6 md:mt-24 max-w-5xl justify-center"
       >
         <div className="sm:order-2 basis-1/3">
           <ProfileCard className="w-full min-w-sm max-w-lg rounded-lg shadow" />
         </div>
-        <div className="m-24 basis-2/3 sm:order-1">
+        <div className="my-12 sm:m-12 basis-2/3 sm:order-1">
           <h1>
             <span className="text-3xl font-bold">{LANDING_PAGE.greeting}</span>
           </h1>
--- src/components/ExoticNavbar/index.tsx
+++ src/components/ExoticNavbar/index.tsx
@@ -3,13 +3,13 @@ import Bunny from "../Icons/bunnysden.svg";
 
 export default function ExoticNavbar() {
   return (
-    <div className="flex font-ubuntu font-bold m-12 align-middle">
+    <div className="flex font-ubuntu font-bold m-6 sm:m-12">
       <nav className="w-full">
         <div className="flex-1 absolute">
-          <Bunny className="w-32 h-32" />
+          <Bunny className="w-16 h-16 sm:w-24 sm:h-24 md:w-32 md:h-32" />
         </div>
-        <div className="flex-1 flex items-center text-3xl mt-12 space-x-32 justify-center space-x-">
-          {/* <ExoticLink
+        <div className="flex-1 flex sm:text-xl md:text-3xl sm:mt-12 mt-6 space-x-12 sm:space-x-16 md:space-x-28 sm:justify-center justify-end space-x-">
+          <ExoticLink
             href="/"
             className={
               "block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4"
@@ -29,7 +29,7 @@ export default function ExoticNavbar() {
               "block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4"
             }
             destination={"Projects"}
-          /> */}
+          />
         </div>
       </nav>
     </div>
