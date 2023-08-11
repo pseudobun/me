6bea0e45e88d64c2b334bca7b33638be55103197
Urban Vidovič <urbanfoundit@gmail.com>
2023-08-11 02:56:29 +0200

    chore: configure wasm loading
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/portfolio/next.config.js
+++ packages/portfolio/next.config.js
@@ -4,6 +4,11 @@ const StylelintPlugin = require("stylelint-webpack-plugin");
 const nextConfig = {
   webpack: (config) => {
     config.plugins.push(new StylelintPlugin());
+    const experiments = config.experiments || {};
+    config.experiments = {
+      ...experiments,
+      asyncWebAssembly: true,
+    };
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
