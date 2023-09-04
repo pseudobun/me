5391f55a40e73ec8c6a63f1f1313c38eb9e06426
Urban Vidovič <urbanfoundit@gmail.com>
2023-09-04 20:17:18 +0200

    feat: add blog link to navigation

--- packages/portfolio/src/components/Menu/index.tsx
+++ packages/portfolio/src/components/Menu/index.tsx
@@ -1,4 +1,5 @@
-import { MENUS } from "../../config";
+import { MENUS, MenuInput } from "../../config";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import { Transition } from "@headlessui/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -21,17 +22,32 @@ export default function Menu({ isOpen }: MenuProps) {
       className="absolute inset-0 z-10 flex flex-col bg-gradient-to-tr from-black to-gray-800"
     >
       <div className="flex flex-1 flex-col items-center justify-center space-y-4 text-2xl">
-        {MENUS.map((menu) => (
-          <Link
-            key={menu.label}
-            href={menu.href}
-            className={`hover:text-wave hover:opacity-80 active:opacity-50 ${
-              pathname === menu.href ? "text-wave underline" : ""
-            }`}
-          >
-            {menu.label}
-          </Link>
-        ))}
+        {MENUS.map((menu: MenuInput) =>
+          menu?.external ? (
+            <div key={menu.label} className="flex space-x-2">
+              <a
+                key={menu.label}
+                href={menu.href}
+                target="_blank"
+                className={`underline hover:text-wave hover:opacity-80 
+              active:opacity-50`}
+              >
+                {menu.label}
+              </a>
+              <ArrowTopRightOnSquareIcon width={20} />
+            </div>
+          ) : (
+            <Link
+              key={menu.label}
+              href={menu.href}
+              className={`hover:text-wave hover:opacity-80 active:opacity-50 ${
+                pathname === menu.href ? "text-wave underline" : ""
+              }`}
+            >
+              {menu.label}
+            </Link>
+          )
+        )}
       </div>
     </Transition>
   );
--- packages/portfolio/src/config.ts
+++ packages/portfolio/src/config.ts
@@ -1,3 +1,8 @@
+export type MenuInput = {
+  label: string;
+  href: string;
+  external?: boolean;
+};
 export const MENUS = [
   {
     label: "Home",
@@ -7,10 +12,11 @@ export const MENUS = [
     label: "About",
     href: "/about",
   },
-  // {
-  //   label: "Projects",
-  //   href: "/projects",
-  // },
+  {
+    label: "Blog",
+    href: "https://mirror.xyz/0x32B1172E786a31A65b46710Cd946b2521e13ac96",
+    external: true,
+  },
   // {
   //   label: "Contact",
   //   href: "/contact",
