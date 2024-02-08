e9fc6c587f9788be0ed6fbeacd2cbdcedec8642c
Urban Vidovič <urbanfoundit@gmail.com>
2024-02-08 15:07:26 +0100

    chore: remove sitemap config
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/portfolio/next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-  siteUrl: 'https://pseudobun.dev',
-  generateRobotsTxt: true,
-  generateIndexSitemap: false,
-  sitemapSize: 1000,
-};
-
-module.exports = config;
