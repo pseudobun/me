5cf137e023c373419cf2d6b3261d119b65aa58b8
Urban Vidovič <urbanfoundit@gmail.com>
2024-04-05 16:32:07 +0200

    chore: add sitemap generation
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

---
+++ packages/portfolio/next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://pseudobun.dev',
+  generateRobotsTxt: false,
+  generateIndexSitemap: true,
+  sitemapSize: 1000,
+};
+
+module.exports = config;
