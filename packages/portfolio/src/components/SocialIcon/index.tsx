3b9283ff32d2f8eb2009d3e740c0a6a20a282abb
Urban Vidovič <urbanfoundit@gmail.com>
2024-04-09 10:23:03 +0200

    chore: replace hey png with svg
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/portfolio/src/components/SocialIcon/index.tsx
+++ packages/portfolio/src/components/SocialIcon/index.tsx
@@ -5,9 +5,8 @@ import Github from '../Icons/github.svg';
 import Mail from '../Icons/gmail.svg';
 import LinkedIn from '../Icons/linkedin.svg';
 import Telegram from '../Icons/telegram.svg';
-import Image from 'next/image';
 import Twitter from '../Icons/twitter.svg';
-import Hey from '../Icons/hey.png';
+import Hey from '../Icons/hey.svg';
 import Link from 'next/link';
 // import Hey from '../Icons/hey.svg';
 
@@ -42,23 +41,7 @@ export default function SocialIcon({ className, icon, href }: SocialIconProps) {
     href = `mailto:${href}`;
   }
 
-  return icon === 'hey' ? (
-    <Link
-      aria-label={`link to ${icon}`}
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={clsx(className)}
-    >
-      <Image
-        width={32}
-        height={32}
-        placeholder="blur"
-        src={Icon}
-        alt="Hey icon"
-      />
-    </Link>
-  ) : (
+  return (
     <Link
       aria-label={`link to ${icon}`}
       href={href}
