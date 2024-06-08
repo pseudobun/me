76173ade7f7ef598495add283dbd349c15404418
Urban Vidovič <urbanfoundit@gmail.com>
2024-06-08 15:32:12 +0200

    feat: add flippable project cards
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/portfolio/src/components/ProjectCard/index.tsx
+++ packages/portfolio/src/components/ProjectCard/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+'use client';
+import type React from 'react';
+import { useState } from 'react';
 import Image, { type StaticImageData } from 'next/image';
+import ReactCardFlip from 'react-card-flip';
 import {
   ArrowTopRightOnSquareIcon,
   GlobeAltIcon,
@@ -34,43 +37,68 @@ export default function ProjectCard({
   orgUrl,
   image,
 }: ProjectCardProps) {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
   return (
-    <Card className="w-full dark">
-      <CardHeader className="flex gap-3 h-[4rem]">
-        <div className="flex flex-col">
-          <p className="text-lg sm:text-xl">{title}</p>
-          <Link href={orgUrl} target="_blank" className="gap-x-1">
-            <p className="text-sm sm:text-md text-wave">{org}</p>
-            <ArrowTopRightOnSquareIcon className="text-wave" width={12} />
-          </Link>
-        </div>
-      </CardHeader>
-      <Divider />
-      <CardBody>
-        <Image
-          alt="nextui logo"
-          width={1024}
-          src={image}
-          placeholder="blur"
-          className="rounded-lg"
-        />
-        <ScrollShadow className="min-h-[64px] h-[128px] m-2 no-scrollbar text-md sm:text-lg">
-          <p className="pt-2">{description}</p>
-        </ScrollShadow>
-      </CardBody>
-      <Divider />
-      <CardFooter className="justify-end gap-x-2 h-[3rem]">
-        {website && (
-          <Link href={website} target="_blank">
-            <GlobeAltIcon color="white" className={'h-[36px] w-[36px]'} />
-          </Link>
-        )}
-        {github && (
-          <Link href={github} target="_blank">
-            <Github className={'h-[32px] w-[32px]'} />
-          </Link>
-        )}
-      </CardFooter>
-    </Card>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      {/* Front face */}
+      <Card className="dark h-96 w-full" isPressable onPress={handleClick}>
+        <CardHeader className="flex gap-3 h-[4rem] text-left">
+          <div className="flex flex-col">
+            <p className="text-lg sm:text-xl">{title}</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <Image
+            alt="nextui logo"
+            width={480}
+            src={image}
+            placeholder="blur"
+            className="rounded-lg"
+          />
+        </CardBody>
+        <Divider />
+        <CardFooter className="justify-end gap-x-2 h-[3rem]">
+          {website && (
+            <Link href={website} target="_blank">
+              <GlobeAltIcon color="white" className={'h-[36px] w-[36px]'} />
+            </Link>
+          )}
+          {github && (
+            <Link href={github} target="_blank">
+              <Github className={'h-[32px] w-[32px]'} />
+            </Link>
+          )}
+        </CardFooter>
+      </Card>
+
+      {/* Back face */}
+      <Card className="h-96 w-full dark" isPressable onPress={handleClick}>
+        <CardHeader className="flex gap-3 h-[4rem] text-left">
+          <div className="flex flex-col">
+            <p className="text-lg sm:text-xl">Description</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <ScrollShadow className="m-2 no-scrollbar text-md sm:text-lg">
+            <p className="pt-2">{description}</p>
+          </ScrollShadow>
+        </CardBody>
+        <Divider />
+        <CardFooter className="justify-start gap-x-2 h-[3rem]">
+          <p className="text-sm sm:text-md">
+            Developed at{' '}
+            <Link href={orgUrl} target="_blank" className="gap-x-1">
+              <span className="text-sm sm:text-md text-wave">{org}</span>
+              <ArrowTopRightOnSquareIcon className="text-wave" width={12} />
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </ReactCardFlip>
   );
 }
