b62ca0eb82a18f72bc0fa070a1c8b27c0067d66b
Urban Vidovič <urbanfoundit@gmail.com>
2024-06-11 00:26:58 +0200

    chore: typo in gallery css
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/portfolio/src/app/gallery/loading.tsx
+++ packages/portfolio/src/app/gallery/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from '@nextui-org/react';
 
 export default function Loading() {
   return (
-    <div className="flesx flex-col items-center justify-center h-full">
+    <div className="flex flex-col items-center justify-center h-full">
       <Spinner size="lg" />
     </div>
   );
