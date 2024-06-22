f124933d61bea0153e22fa09dcec731040eb2e3c
Urban Vidovič <urbanfoundit@gmail.com>
2024-06-22 16:24:43 +0200

    fix: build issues
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/portfolio/src/config/supabase.ts
+++ packages/portfolio/src/config/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseClient = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_URL! || 'https://supabase.co',
   process.env.SUPABASE_ANON_PUBLIC!
 );
 
