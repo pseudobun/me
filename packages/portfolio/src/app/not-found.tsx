ce459b3ef14bd557c7658ef7d890bcdee7da6c68
Urban Vidovič <urbanfoundit@gmail.com>
2024-10-22 18:29:09 +0200

    chore: remove logo from not found page
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/portfolio/src/app/not-found.tsx
+++ packages/portfolio/src/app/not-found.tsx
@@ -1,13 +1,6 @@
-import { MemoizedSVG } from '@/components/MemoizedSVG';
-
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-full w-full">
-      <MemoizedSVG
-        aria-label="Bunnys Den logo"
-        src="/bunnysden.svg"
-        className="h-4/5 w-4/5 flex-1 md:h-1/3 md:w-1/3"
-      />
       <p className="text-md text-center md:text-xl text-stone-200">
         404: You ain't gonna find anything here
       </p>
