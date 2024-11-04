15044f17d8d574e79e3c0c5f73b514aae489ebd0
Urban Vidovič <urbanfoundit@gmail.com>
2024-11-04 16:39:03 +0100

    chore: break nav on too narrow
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/portfolio/src/components/Navigation/index.tsx
+++ packages/portfolio/src/components/Navigation/index.tsx
@@ -1,5 +1,3 @@
-'use client';
-import { useEffect, useState } from 'react';
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -7,81 +5,37 @@ import {
 } from '@/components/ui/navigation-menu';
 import { type MenuInput, MENUS } from '@/config/menu';
 import Link from '@/components/Link';
-import {
-  Link as LinkIcon,
-  Menu as MenuIcon,
-  X as CloseIcon,
-} from 'lucide-react';
-import { ThemeToggle } from '../ThemeToggle';
-import { MemoizedSVG } from '../MemoizedSVG';
+import { Link as LinkIcon } from 'lucide-react';
+import { MemoizedSVG } from '@/components/MemoizedSVG';
+import { ThemeToggle } from '@/components/ThemeToggle';
 
 export default function Navigation() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const handleLinkClick = () => {
-    setSidebarOpen(false);
-  };
-
-  useEffect(() => {
-    if (isSidebarOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = '';
-    }
-    return () => {
-      document.body.style.overflow = '';
-    };
-  }, [isSidebarOpen]);
-
   return (
-    <header className="flex sm:flex-row">
+    <header className="flex justify-between items-center gap-x-12 p-4 w-full">
       <div className="flex items-center">
-        <button
-          type="button"
-          className="sm:hidden max-sm:p-4"
-          onClick={() => setSidebarOpen(!isSidebarOpen)}
-        >
-          <MenuIcon className="h-6 w-6 text-cappuccino" />
-        </button>
+        <MemoizedSVG
+          aria-label="Bunnys Den logo"
+          src="bunnysden.svg"
+          className="h-8 w-8 flex-1"
+        />
       </div>
-      <div
-        className={`fixed max-sm:flex max-sm:flex-col top-0 left-0 w-full h-full bg-background p-4 transform ${
-          isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
-        } w-full justify-between items-center transition-transform sm:relative sm:translate-x-0 sm:flex sm:items-center sm:bg-transparent`}
-      >
-        <button
-          type="button"
-          className="absolute top-4 right-4 sm:hidden"
-          onClick={handleLinkClick}
-        >
-          <CloseIcon className="h-6 w-6 text-cappuccino" />
-        </button>
-        <div className="flex items-center">
-          <MemoizedSVG
-            aria-label="Bunnys Den logo"
-            src="bunnysden.svg"
-            className="h-8 w-8 flex-1 hidden sm:block"
-          />
-        </div>
-        <NavigationMenu>
-          <NavigationMenuList className="flex flex-col sm:flex-row sm:gap-x-12">
-            {MENUS.map((menu: MenuInput) => (
-              <NavigationMenuItem key={menu.label}>
-                <Link
-                  href={menu.href}
-                  isExternal={menu.external ? true : undefined}
-                  onClick={handleLinkClick}
-                >
-                  {menu.label}
-                  {menu.external ? <LinkIcon className="w-3 h-3" /> : null}
-                </Link>
-              </NavigationMenuItem>
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu>
-        <div>
-          <ThemeToggle />
-        </div>
+      <NavigationMenu>
+        <NavigationMenuList className="flex-wrap max-md:gap-x-4 gap-x-12">
+          {MENUS.map((menu: MenuInput) => (
+            <NavigationMenuItem key={menu.label}>
+              <Link
+                href={menu.href}
+                isExternal={menu.external ? true : undefined}
+              >
+                {menu.label}
+                {menu.external ? <LinkIcon className="w-3 h-3" /> : null}
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div>
+        <ThemeToggle />
       </div>
     </header>
   );
